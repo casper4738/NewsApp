2e7ffa40332c6b39e6f98c21be35087a25f41df8
Fandy <fandyadam91@gmail.com>
2021-11-17 13:11:50 +0700

    add action bottom navigation

--- app/src/main/java/com/fandy/news/ui/MainActivity.kt
+++ app/src/main/java/com/fandy/news/ui/MainActivity.kt
@@ -3,11 +3,17 @@ package com.fandy.news.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate.*
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import androidx.preference.PreferenceManager
 import com.fandy.news.R
 import com.fandy.news.databinding.ActivityMainBinding
 import com.fandy.news.util.PREF_MODE_KEY
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -21,9 +27,18 @@ class MainActivity : AppCompatActivity() {
          * Ensures that the SharedPreferences file is properly initialized with
          * the default values when this method is called for the first time.
          */
-        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false)
+//        PreferenceManager.setDefaultValues(this, R.xml.root_preferences, false)
         setupMode()
+
+        val host: NavHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment? ?: return
+        val navController = host.navController
+
+        //Initialize the bottom navigation view
+        //create bottom navigation view object
+        binding.bottomNavigation.setupWithNavController(navController)
     }
+
 
     /**
      * Get the user's mode settings from SharedPreferences.
@@ -34,4 +49,5 @@ class MainActivity : AppCompatActivity() {
         val isNightMode = sharedPreferences.getBoolean(PREF_MODE_KEY, false)
         setDefaultNightMode(if (isNightMode) MODE_NIGHT_YES else MODE_NIGHT_NO)
     }
+
 }

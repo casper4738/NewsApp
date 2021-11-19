c752cf416663a520af6f391dc65facc257837326
Fandy <fandyadam91@gmail.com>
2021-11-19 15:44:41 +0700

    remove yang tidak digunakan

--- app/src/main/java/com/fandy/news/ui/headline/HeadlineArticleAdapter.kt
+++ app/src/main/java/com/fandy/news/ui/headline/HeadlineArticleAdapter.kt
@@ -20,88 +20,35 @@ class HeadlineArticleAdapter :
     PagingDataAdapter<ArticleTopHeadlines, RecyclerView.ViewHolder>(DIFF_CALLBACK) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        if (viewType == 0) {
-            val layoutInflater = LayoutInflater.from(parent.context)
-            val binding = ArticleItemMainBinding.inflate(layoutInflater, parent, false)
-            return HeadlineArticleItemMainViewHolder(binding)
-        } else {
-            val layoutInflater = LayoutInflater.from(parent.context)
-            val binding = ArticleItemOtherBinding.inflate(layoutInflater, parent, false)
-            return HeadlineArticleItemOtherViewHolder(binding)
-        }
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = ArticleItemOtherBinding.inflate(layoutInflater, parent, false)
+        return HeadlineArticleItemOtherViewHolder(binding)
     }
 
     override fun onBindViewHolder(viewHolder: RecyclerView.ViewHolder, position: Int) {
         val article = getItem(position) ?: return
-        if (getItemViewType(position) == 0) {
-            (viewHolder as HeadlineArticleItemMainViewHolder).bind(article)
-        } else {
-            (viewHolder as HeadlineArticleItemOtherViewHolder).bind(article)
-        }
-    }
-
-
-    override fun getItemViewType(position: Int): Int {
-        return 1
+        (viewHolder as HeadlineArticleItemOtherViewHolder).bind(article)
     }
 
 
     companion object {
         val DIFF_CALLBACK: DiffUtil.ItemCallback<ArticleTopHeadlines> =
             object : DiffUtil.ItemCallback<ArticleTopHeadlines>() {
-                override fun areItemsTheSame(oldItem: ArticleTopHeadlines, newItem: ArticleTopHeadlines) =
+                override fun areItemsTheSame(
+                    oldItem: ArticleTopHeadlines,
+                    newItem: ArticleTopHeadlines
+                ) =
                     oldItem == newItem
 
-                override fun areContentsTheSame(oldItem: ArticleTopHeadlines, newItem: ArticleTopHeadlines) =
+                override fun areContentsTheSame(
+                    oldItem: ArticleTopHeadlines,
+                    newItem: ArticleTopHeadlines
+                ) =
                     oldItem.id == newItem.id
             }
     }
 
 }
-
-class HeadlineArticleItemMainViewHolder(private val binding: ArticleItemMainBinding) :
-    RecyclerView.ViewHolder(binding.root) {
-
-    fun bind(articleItem: ArticleTopHeadlines) {
-        binding.run {
-            title.loadOrGone(articleItem.title.formatTitle())
-            source.loadOrGone(articleItem.source.name)
-            publishedAt.loadOrGone(articleItem.date.formatDate())
-            articleImage.loadImageOrDefault(articleItem.imgUrl)
-
-            setOnClickListener(articleItem)
-        }
-    }
-
-    private fun setOnClickListener(articleItem: ArticleTopHeadlines) {
-        binding.detailItem.setOnClickListener { view ->
-            navigateToDetail(articleItem, view)
-        }
-    }
-
-    private fun navigateToDetail(articleItem: ArticleTopHeadlines, view: View) {
-        val directions =
-            HeadlineFragmentDirections.actionHeadlineFragmentToArticleFragment(Article(
-                id = articleItem.id,
-                url = articleItem.url,
-                author = articleItem.author,
-                title = articleItem.title,
-                description = articleItem.description,
-                imgUrl = articleItem.imgUrl,
-                date = articleItem.date,
-                content = articleItem.content,
-                source = articleItem.source,
-                category = articleItem.category,
-                language = articleItem.language,
-                keyword = articleItem.keyword,
-                from_date = articleItem.from_date,
-                to_date = articleItem.to_date
-            ))
-        view.findNavController().navigate(directions)
-    }
-
-}
-
 
 class HeadlineArticleItemOtherViewHolder(private val binding: ArticleItemOtherBinding) :
     RecyclerView.ViewHolder(binding.root) {
@@ -125,22 +72,24 @@ class HeadlineArticleItemOtherViewHolder(private val binding: ArticleItemOtherBi
 
     private fun navigateToDetail(articleItem: ArticleTopHeadlines, view: View) {
         val directions =
-            HeadlineFragmentDirections.actionHeadlineFragmentToArticleFragment(Article(
-                id = articleItem.id,
-                url = articleItem.url,
-                author = articleItem.author,
-                title = articleItem.title,
-                description = articleItem.description,
-                imgUrl = articleItem.imgUrl,
-                date = articleItem.date,
-                content = articleItem.content,
-                source = articleItem.source,
-                category = articleItem.category,
-                language = articleItem.language,
-                keyword = articleItem.keyword,
-                from_date = articleItem.from_date,
-                to_date = articleItem.to_date
-            ))
+            HeadlineFragmentDirections.actionHeadlineFragmentToArticleFragment(
+                Article(
+                    id = articleItem.id,
+                    url = articleItem.url,
+                    author = articleItem.author,
+                    title = articleItem.title,
+                    description = articleItem.description,
+                    imgUrl = articleItem.imgUrl,
+                    date = articleItem.date,
+                    content = articleItem.content,
+                    source = articleItem.source,
+                    category = articleItem.category,
+                    language = articleItem.language,
+                    keyword = articleItem.keyword,
+                    from_date = articleItem.from_date,
+                    to_date = articleItem.to_date
+                )
+            )
         view.findNavController().navigate(directions)
     }
 
